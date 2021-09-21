c2376d2103c8237e240f98e19d59f715b5967950
Debojyoti Roy <debojyotiroy245@gmail.com>
2021-09-22 03:14:12 +0530

    features added: next and reset button

--- main.js
+++ main.js
@@ -1,27 +1,47 @@
 const billAmount = document.querySelector("#bill-amount")
 const cashGiven = document.querySelector("#cash-given")
+const cashGivenLabel = document.querySelector("#cash-given-label")
 const checkButton = document.querySelector("#check-button")
 const message = document.querySelector("#error-message")
+const returnTable = document.querySelector(".return-table")
 const noOfNotes = document.querySelectorAll(".no-of-notes")
+const nextButton = document.querySelector("#next-button")
+const resetButton = document.querySelector("#reset-button")
 const notes = [2000,500,100,20,10,5,1]
 
-checkButton.addEventListener("click", function validateBillAndCashAmount(){
+hideAllExceptBill()
+
+nextButton.addEventListener("click", function validateBillAmount(){
     hideMessage()
     if(billAmount.value>0){
-        if(cashGiven.value>=billAmount.value){
-            const amountToBeReturned = cashGiven.value - billAmount.value
-            calculateChange(amountToBeReturned)
-        }
-        else{
-            showMessage("Do you wanna wash plates")
-        }
+        nextButton.style.display = "none"
+        cashGiven.style.display = "initial"
+        cashGivenLabel.style.display = "block"
+        checkButton.style.display = "block"
+        
+        checkButton.addEventListener("click", function validateCashAmount(){
+            if(cashGiven.value>=billAmount.value){
+                const amountToBeReturned = cashGiven.value - billAmount.value
+                calculateChange(amountToBeReturned)
+            }
+            else{
+                showMessage("Cash amount should be greater than equal to bill amount")
+                nextButton.style.display = "block"
+                hideAllExceptBill()
+            }
+        })
     }
     else{
-        showMessage("Invalid Bill Amount")
+        showMessage("Bill amount should be greater than zero")
     }
 })
 
+resetButton.addEventListener("click", function(){
+    location.reload()
+})
+
 function calculateChange(amountToBeReturned){
+    returnTable.style.display = "table"
     for(let i=0;i<notes.length;i++){
         const numberOfNotes = Math.trunc(amountToBeReturned/notes[i])
         amountToBeReturned %= notes[i]
@@ -36,4 +56,11 @@ function hideMessage(){
 function showMessage(msg){
     message.style.display = "block"
     message.innerText = msg
+}
+
+function hideAllExceptBill(){
+    cashGiven.style.display = "none"
+    cashGivenLabel.style.display = "none"
+    checkButton.style.display = "none"
+    returnTable.style.display = "none"
 }
